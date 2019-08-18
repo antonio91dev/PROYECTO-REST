95462afd33b4a17b03187d8cad8b7d298f92fb8e
Jesus Antonio <jesus.antonio@mdp.com.pe>
2019-08-18 11:34:32 -0500

    [INIT] Inicio de Proyecto Rest - Control de Personas

---
+++ proyecto-rest-spring/src/main/java/com/control/personal/empresa/ProyectoRestSpringApplication.java
@@ -0,0 +1,13 @@
+package com.control.personal.empresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoRestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoRestSpringApplication.class, args);
+	}
+
+}
---
+++ proyecto-rest-spring/src/test/java/com/control/personal/empresa/ProyectoRestSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.control.personal.empresa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProyectoRestSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
