f2049532c28c7a01910ea5c2551cc73a7ac6a12f
Jesus Antonio <jesus.antonio@mdp.com.pe>
2019-08-19 10:51:33 -0500

    [FEAT] Agregando CommandLineRunner Init

--- proyecto-rest-spring/src/main/java/com/control/personal/empresa/ProyectoRestSpringApplication.java
+++ proyecto-rest-spring/src/main/java/com/control/personal/empresa/ProyectoRestSpringApplication.java
@@ -1,7 +1,11 @@
 package com.control.personal.empresa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.control.personal.empresa.repositorios.EmpleadoRepository;
 
 @SpringBootApplication
 public class ProyectoRestSpringApplication {
@@ -9,5 +13,20 @@ public class ProyectoRestSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoRestSpringApplication.class, args);
 	}
+	
+	
+	/**
+	 * Este bean se inicia al lanzar la aplicación. 
+	 * 
+	 * @param empleadoRepository 
+	 * @return
+	 */
+	@Bean
+    CommandLineRunner init(EmpleadoRepository empleadoRepository) {
+        return (args) -> {
+            //Empleado empleado = new Empleado("123123","nombre 213123", LocalDate.now());
+            //empleadoRepository.save(empleado);
+        };
+    }
 
 }
